9e23ed35e39ed37627caf9f60bbefc376e81376c
APWaldo <apw2131@columbia.edu>
2014-04-24 16:02:17 -0400

    Loading the data based on year selection, 1990 and 2000.

--- scripts.js
+++ scripts.js
@@ -15,6 +15,30 @@ console.log("Hi there. About to make a pretty cool visualization about Finance J
 
 
 
+//Instead of adding data from a static json file, I'm going to load it from a google fusion table.
+function googleLoaded() {
+	
+	var myURL = History.getState().cleanUrl;
+	var queryArray = myURL.split("?"); //split url on question marjk
+	var defaultYear ="1990";
+	
+	if(queryArray.length>1){
+		//get the query string, break it on the =, and then take the second element.
+		defaultYear = queryArray[1].split("=")[1];
+	}
+	
+	//is there something in the URL that specifies the year?
+
+	console.log("Google has loaded");
+	$(".btn-info").on("click", showNewData);
+	//Time to load data with get function. This will tell my page to go and get this data set and use the function
+	//dataLoaded to render it. this grabs the button with the id that is year_"defaultYear"
+	$("#year_"+defaultYear).click();
+
+}
+
+
+
 //Adding my new function pageLoaded and console logging to make sure that the pageLoaded function activates on
 //document ready. This will eventually load my google visualization.
 function pageLoaded() {
